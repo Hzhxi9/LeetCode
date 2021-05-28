3a3539a262aba1754728bd09053437ece28d0382
huangzx <hzhixi9@gmail.com>
2021-05-29 00:37:09 +0800

    第一个只出现一次的字符 思路二

--- DataStructure/Array/firstNotRepeatingChar.js
+++ DataStructure/Array/firstNotRepeatingChar.js
@@ -4,19 +4,14 @@
  * 题目描述
  * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置, 如果没有则返回-1（需要区分大小写）
  *
- * 示例:
- * 输入: s = [3，32，321]
- * 输出: 321323
- *
  */
 
 /**
- * TODO
  * @param {string} s
  * @returns {number}
  */
 function firstNotRepeatingChar(s) {
-  if (!s || s.length > 10000) return;
+  if (!s || s.length > 10000) return -1;
   const temp = {};
   for (let i = 0, len = s.length; i < len; i++) {
     if (temp[s[i]] !== undefined) {
@@ -25,14 +20,22 @@ function firstNotRepeatingChar(s) {
       temp[s[i]] = 1;
     }
   }
-
-  for(const key in temp){
-    console.log(temp[key],'==temp  ')
-    if(temp[key] === 1){
-      return [key, temp[key]]
-    }
+  for (const key in temp) {
+    if (temp[key] === 1) return temp[key];
   }
-  return -1
+  return -1;
 }
 
-console.log(firstNotRepeatingChar('sss'))
+/**
+ * @param {string} s
+ * @returns {number}
+ */
+function firstNotRepeatingChar(s) {
+  if (!s || s.length > 10000) return -1;
+  for (let i = 0, len = s.length; i < len; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+  }
+  return -1;
+}
+
+console.log(firstNotRepeatingChar("sssb"));
