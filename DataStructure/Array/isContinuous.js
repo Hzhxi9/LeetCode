bd0230edcb86c78e08417cfb52c930dd56aee36d
huangzx <hzhixi9@gmail.com>
2021-05-30 15:00:11 +0800

    扑克牌顺子

--- DataStructure/Array/isContinuous.js
+++ DataStructure/Array/isContinuous.js
@@ -6,9 +6,9 @@
  * 2-10为数字本身，A为1，J为11...大小王可以看成任何数字，可以把它当作0处理。
  *
  * 示例:
- * 输入: nums = [3，32，321]
- * 输出: 321323
- * 
+ * 输入: nums = [1, 2, 3, 4, 0]
+ * 输出: true
+ *
  * 思路分析
  * 1. 数组排序
  * 2. 遍历数组
@@ -19,7 +19,55 @@
  */
 
 /**
+ * @param {number[]} nums
+ * @returns {boolean}
+ */
+const isContinuous = function (nums) {
+  const sort = nums.sort((a, b) => a - b);
+  let zero = 0,
+    space = 0;
+  for (let i = 0, len = sort.length - 1; i < len; i++) {
+    /**当元素为零时, 记录0的个数 */
+    if (!sort[i]) {
+      zero++;
+    } else {
+      /**当元素不为0时, 记录间隔数 */
+      const current = sort[i + 1] - sort[i];
+      if (!current) {
+        /** 元素相等,不能构成顺子 */
+        return false;
+      } else {
+        /**TODO */
+        space += current - 1;
+      }
+    }
+  }
+  /**比较间隔数与零的个数 */
+  return zero - space >= 0;
+};
+
+/**
  * 
  * @param {number[]} nums 
+ * @returns {boolean}
  */
-const isContinuous = function (nums) {};
+const isContinuous = function (nums) {
+  if (!nums || !nums.length) return;
+  const s = new Set();
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (!nums[i]) {
+      continue;
+    }
+    if (!s.has(nums[i])) {
+      s.add(nums[i]);
+    } else {
+      return false;
+    }
+  }
+  /** 数组最大值 - 数组最小值 小于等于 4 则满足要求 */
+  console.log(s)
+  return Math.max(...s) - Math.min(...s) <= 4;
+};
+
+/**Debug */
+console.log(isContinuous([7, 2, 3, 4, 6]));
