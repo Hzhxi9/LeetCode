84cbad0cfe481fb85132fd825cd762caa1dedeef
huangzx <hzhixi9@gmail.com>
2021-06-03 09:32:52 +0800

    shellSort

--- Algorithm/sort/shellSort.js
+++ Algorithm/sort/shellSort.js
@@ -2,6 +2,11 @@
  *  希尔排序
  *
  *  思想
+ *  1. 分组
+ *  2. 组内排序
+ *  3. 重新设置间隔分组，间隔变小
+ *  4. 最后间隔为1
+ *
  *  先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，具体算法描述：
  *  1. 选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
  *  2. 按增量序列个数k，对序列进行k 趟排序；
@@ -15,12 +20,14 @@
  */
 const shellSort = function (nums) {
   let len = nums.length;
+  /**增量 gap 每次增量都是上一次增量 / 2 */
   for (let gap = Math.floor(len / 2); gap > 0; gap = Math.floor(gap / 2)) {
     for (let i = 0; i < len; i++) {
       let j = i,
         current = nums[i];
 
-      while (j - gap >= 0 && current <= nums[j - gap]) {
+      while (j - gap >= 0 && nums[j - gap] > current) {
+        /**进行插入排序 */
         nums[j] = nums[j - gap];
         j = j - gap;
       }
@@ -30,5 +37,24 @@ const shellSort = function (nums) {
   return nums;
 };
 
+/**
+ * @param {number[]} nums
+ * @returns {number[]}
+ */
+const shellSort = function (nums) {
+  let len = nums.length;
+  for (let gap = Math.floor(len / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    for (let i = 0; i < len; i++) {
+      let target = i,
+        current = nums[i];
+      while (nums[target - gap] >= 0 && nums[target - gap] > current) {
+        [nums[target - gap], nums[target]] = [nums[target], nums[target - gap]];
+        target = target - gap;
+      }
+    }
+  }
+  return nums;
+};
+
 /**Debug */
-console.log(shellSort([10, 50, 30, 90, 40, 80, 70]));
+console.log(shellSort([10, 50, 30, 90, 40, 80, 70, 50]));
