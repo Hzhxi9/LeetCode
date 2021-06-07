72d287f40cb7709e25c66c115890dd97688a6f22
huangzx <hzhixi9@gmail.com>
2021-06-07 08:59:04 +0800

    解法一

--- Algorithm/Math/countDigitOne.js
+++ Algorithm/Math/countDigitOne.js
@@ -26,25 +26,70 @@
  *  则1在万位上出现的个数为 a * base
  */
 
+/**
+ * 解法一
+ * 包含1的个数
+ * @param {number} n
+ * @returns {number}
+ */
 const countDigitOne = function (n) {
-  let base = 1,
-    res = 1,
-    a = 1,
-    b = 1,
-    cur = 1;
-
-  while (base <= n) {
-    b = n % base;
-    a = n / base;
-    cur = a % 10;
-    a /= 10;
-    if (cur > 1) res += (a + 1) * base;
-    else if (cur === 1) res += (a * base + b + 1)
-    else res += a * base;
-    base *= 10;
+  let count = 0;
+  for (let i = 0; i <= n; i++) {
+    count += numberOfOne(i);
   }
-
-  return res;
+  return count;
 };
 
-console.log(countDigitOne(13))
+/**
+ * 查找是否包含1
+ * @param {number} n
+ * @returns {number}
+ */
+function numberOfOne(n) {
+  let count = 0;
+  let pos = n.toString().indexOf(1);
+  while (~pos) {
+    pos = n.toString().indexOf(1, pos + 1);
+    count++;
+  }
+  return count;
+}
+
+console.log(countDigitOne(1000));
+
+// const countDigitOne = function (n) {
+//   let i = 1,
+//     count = 0,
+//     current,
+//     before,
+//     after;
+
+//   while (n / i !== 0) {
+//     current = (n / i) % 10;
+//     before = n / (i * 10);
+//     after = n - (n / i) * i;
+
+//     if (current === 0) {
+//       count += before * i;
+//     } else if (current === 1) {
+//       count = count + (before * i + after + 1);
+//     } else {
+//       count += (before + 1) * i;
+//     }
+
+//     i = i * 10;
+
+//     // b = n % base;
+//     // a = n / base;
+//     // cur = a % 10;
+//     // a /= 10;
+//     // if (cur > 1) res += (a + 1) * base;
+//     // else if (cur === 1) res += a * base + b + 1;
+//     // else res += a * base;
+//     // base *= 10;
+//   }
+
+//   return count;
+// };
+
+// console.log(countDigitOne(13));
