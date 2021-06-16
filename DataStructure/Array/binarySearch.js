b889b56f376951a48224a15f7d697429dcf8cb27
huangzx <hzhixi9@gmail.com>
2021-06-16 16:24:09 +0800

    注释

--- DataStructure/Array/binarySearch.js
+++ DataStructure/Array/binarySearch.js
@@ -19,39 +19,44 @@
 /**
  *  使用二分查找找到目标值，在向前向后遍历，找到所有的数
  */
-// const binarySearch = (array, target) => {
-//   if (!array.length) return 0;
-//   const len = array.length;
+const binarySearch = (array, target) => {
+  if (!array.length) return 0;
+  const len = array.length;
 
-//   let l = 0,
-//     r = len - 1;
+  let l = 0,
+    r = len - 1;
 
-//   while (l < r) {
-//     let mid = ~~((l + r) >> 1);
-//     if (array[mid] < target) {
-//       l = mid + 1;
-//     } else {
-//       r = mid;
-//     }
-//   }
+  while (l < r) {
+    let mid = ~~((l + r) >> 1);
+    if (array[mid] < target) {
+      l = mid + 1;
+    } else {
+      r = mid;
+    }
+  }
 
-//   if (array[l] !== target) return 0;
-//   let left = l;
+  if (array[l] !== target) return 0;
+  let left = l;
 
-//   (l = 0), (r = len - 1);
-//   while (l < r) {
-//     let mid = ~~((l + r + 1) >> 1);
-//     if (array[mid] <= target) {
-//       l = mid;
-//     } else {
-//       r = mid - 1;
-//     }
-//   }
-//   let right = r;
+  (l = 0), (r = len - 1);
+  while (l < r) {
+    let mid = ~~((l + r) >> 1);
+    if (array[mid] <= target) {
+      l = mid;
+    } else {
+      r = mid - 1;
+    }
+  }
+  let right = r;
 
-//   return right - left + 1;
-// };
+  console.log(left, right);
 
+  return right - left + 1;
+};
+
+/**
+ *  使用二分查找找到目标值，在向前向后遍历，找到所有的数
+ */
 // const binarySearch = (array, target) => {
 //   const index = array.indexOf(target);
 
@@ -70,29 +75,35 @@
 //   return len;
 // };
 
-const binarySearch = (nums, target) => {
-  if (!nums.length) return 0;
-  const len = nums.length;
-  /**搜索区间 [l,r] */
-  let l = 0,
-    r = len - 1,
-    count = 0;
+/**
+ *  使用二分查找找到目标值，在向前向后遍历，找到所有的数
+ */
+// const binarySearch = (nums, target) => {
+//   if (!nums.length) return 0;
+//   const len = nums.length;
+//   /**搜索区间 [l,r] */
+//   let l = 0,
+//     r = len - 1,
+//     count = 0;
 
-  while (l <= r) {
-    let mid = ~~((l + r) >> 1);
-    /**搜索区间变为[l,m] */
-    if (nums[mid] >= target) r = mid;
-    /**搜索区间变为[mid+1,r] */
-    else l = mid + 1;
-  }
-  if (nums[l] === target) {
-    for (let i = l; i < len; i++) {
-      if (nums[i] === target) count++;
-      else break;
-    }
-  }
+//   while (l < r) {
+//     let mid = ~~((l + r) >> 1);
 
-  return count;
-};
+//     /**搜索区间变为[l,m] */
+//     if (nums[mid] >= target) r = mid;
+//     /**搜索区间变为[mid+1,r] */ else l = mid + 1;
+//   }
+
+//   if (nums[l] === target) {
+//     for (let i = l; i < len; i++) {
+//       if (nums[i] === target) count++;
+//       else break;
+//     }
+//   }
+
+//   return count;
+// };
+
+console.log(binarySearch([1, 2, 3, 3, 3, 3, 4, 5], 3));
 
 module.exports = binarySearch;
--- DataStructure/Test/Array.test.js
+++ DataStructure/Test/Array.test.js
@@ -1,31 +1,31 @@
-const threeSum = require("../Array/threeSum");
-test.skip("三数之和", () => {
-  expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([
-    [-1, -1, 2],
-    [-1, 0, 1],
-  ]);
-});
+// const threeSum = require("../Array/threeSum");
+// test.skip("三数之和", () => {
+//   expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([
+//     [-1, -1, 2],
+//     [-1, 0, 1],
+//   ]);
+// });
 
-const findInversePairNum = require("../Array/findInversePairNum");
-test.skip("数组中的逆序对", () => {
-  expect(findInversePairNum([1, 2, 3, 4, 5, 6, 7, 0])).toBe(7);
-});
+// const findInversePairNum = require("../Array/findInversePairNum");
+// test.skip("数组中的逆序对", () => {
+//   expect(findInversePairNum([1, 2, 3, 4, 5, 6, 7, 0])).toBe(7);
+// });
 
-const printMatrix = require("../Array/printMatrix");
-test.skip("顺时针打印矩阵", () => {
-  expect(
-    printMatrix([
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-    ])
-  ).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
-});
+// const printMatrix = require("../Array/printMatrix");
+// test.skip("顺时针打印矩阵", () => {
+//   expect(
+//     printMatrix([
+//       [1, 2, 3],
+//       [4, 5, 6],
+//       [7, 8, 9],
+//     ])
+//   ).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+// });
 
 const binarySearch = require("../Array/binarySearch");
 test("在排序数组中查找数字", () => {
-  expect(binarySearch([1, 2, 3, 3, 3, 3, 4, 5], 3)).toBe(4);
-}); 
+  expect(binarySearch([1, 2, 3, 3, 3, 3, 4, 5], 4)).toBe(1);
+});
 
 // const fourSum = require("../Array/fourSum");
 // test("四数之和", () => {
